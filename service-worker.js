0e081e7c0ea037601379d45e56bb5efdca733c70
BERENICE MARTINEZ <berenice1290ma@gmail.com>
2026-03-17 17:48:47 -0600

    Modifications for the cache and improved performance

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,39 @@
+const { cache } = require("react");
+
+const cache_name = "task-app-v1";
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./css/style.css",
+    "./js/app.js",
+    "./manifest.json"
+]
 self.addEventListener("install", e => {
-    console.log("Service worker installed");
+    e.waitUntil(
+        caches.open(cache_name)
+        .then(cache => cache.addAll(urlsToCache))
+    );  
+});
+
+self.addEventListener("active", e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if(key !== cache_name) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(response => {
+            return reponse || fetch(e.request);
+        })
+    );
 });
